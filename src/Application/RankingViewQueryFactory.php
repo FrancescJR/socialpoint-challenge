f472c0b01d2bfe073493c1eb23e435d786701259
Cesc <francesc.travesa@gmail.com>
2023-02-12 10:37:32 +0100

    Adds query factory and finishes controller

---
+++ src/Application/RankingViewQueryFactory.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cesc\Ranking\Application;
+
+use Exception;
+
+final class RankingViewQueryFactory
+{
+    public static function fromString(string $queryString): RankingViewQuery
+    {
+        if (str_starts_with($queryString, "top")) {
+            return self::getTopRankingQueryFromString($queryString);
+        }
+
+        if (str_starts_with($queryString, "at")) {
+            return self::relativeRankingQueryFromString($queryString);
+        }
+        throw new Exception("Could not get any query for string" . $queryString);
+    }
+
+    /**
+     * @param string $queryString
+     * @return TopRankingViewQuery
+     * @throws Exception
+     */
+    public static function getTopRankingQueryFromString(string $queryString): TopRankingViewQuery
+    {
+        if (!str_starts_with($queryString, "top")) {
+            throw new Exception("query must start with top");
+        }
+        $top = substr($queryString, 3);
+        if (!ctype_digit($top)) {
+            throw new Exception("Query string after Top must contain a number only");
+        }
+        return new TopRankingViewQuery((int)$top);
+    }
+
+    /**
+     * @param string $queryString
+     * @return RelativeRankingViewQuery
+     * @throws Exception
+     */
+    public static function relativeRankingQueryFromString(string $queryString): RelativeRankingViewQuery
+    {
+        if (!str_starts_with($queryString, "at")) {
+            throw new Exception("query must start with at");
+        }
+        $at = substr($queryString, 2);
+        $values = explode($at, "/");
+        if(count($values) !== 2 ) {
+            throw new Exception("Query string after at must be with the format xx/x");
+        }
+        if (!ctype_digit($values[0]) || !ctype_digit($values[1])) {
+            throw new Exception("query string with At should have a number, a slash and another number.");
+        }
+        return new RelativeRankingViewQuery(
+            (int) $values[0],
+            (int) $values[1]
+        );
+    }
+}
--- src/Infrastructure/API/RankingViewController.php
+++ src/Infrastructure/API/RankingViewController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Cesc\Ranking\Infrastructure\API;
 
 use Cesc\Ranking\Application\RankingViewQuery;
+use Cesc\Ranking\Application\RankingViewQueryFactory;
 use Cesc\Ranking\Application\RelativeRankingViewQuery;
 use Cesc\Ranking\Application\RelativeRankingViewQueryHandler;
 use Cesc\Ranking\Application\TopRankingViewQuery;
 use Cesc\Ranking\Application\TopRankingViewQueryHandler;
+use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,14 +29,15 @@ final class RankingViewController
     {
         try {
             $query = $this->getQueryFromRequest($request);
+            // straight away strategy pattern
             if (get_class($query) == TopRankingViewQuery::class) {
                 $response = ($this->topRankingQueryHandler)($query);
-            } elseif(get_class($query) == RelativeRankingViewQuery::class) {
+            } elseif (get_class($query) == RelativeRankingViewQuery::class) {
                 $response = ($this->relativeQueryHandler)($query);
             } else {
-                throw new \Exception("Query to view the ranking not understood");
+                throw new Exception("Query to view the ranking not understood");
             }
-        } catch (\Exception $exception) {
+        } catch (Exception $exception) {
             return new JsonResponse([
                 'exception' => get_class($exception),
                 'error' => $exception->getMessage()
@@ -43,9 +46,17 @@ final class RankingViewController
         return new JsonResponse($response);
     }
 
+    /**
+     * @param Request $request
+     * @return RankingViewQuery
+     * @throws Exception
+     */
     public function getQueryFromRequest(Request $request): RankingViewQuery
     {
-        
+        $type = $request->query->get('type');
+        if (!$type) {
+            throw new Exception("Query string must have 'type=xxx'");
+        }
+        return RankingViewQueryFactory::fromString($type);
     }
-
 }
